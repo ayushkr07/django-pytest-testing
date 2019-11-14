70375c333b7c10797cb8e40645c67b39e874dde2
Ayush Kumar <aks010598@gmail.com>
2019-11-14 17:08:39 +0530

    tested views

--- sampleApp/views.py
+++ sampleApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import generic
+
+class HomeView(generic.TemplateView):
+    template_name = 'sampleApp/home.html'
